ffefca66a0d786c865336c07a9da8341d81db9b4
Ghanshyam <gmaliwal777@gmail.com>
2017-12-31 21:26:00 +0530

    Initial Commit

---
+++ GITDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GITDemo
+//
+//  Created by Macmini on 12/31/17.
+//  Copyright © 2017 Macmini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
